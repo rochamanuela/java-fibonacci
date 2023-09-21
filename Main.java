f3dba925271057110dc8146778c1cdbd7d7492b6
Manuela Rocha <117682525+rochamanuela@users.noreply.github.com>
2023-09-21 14:47:25 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int[] fibonacci = new int[20];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+
+        for (int i = 2; i < fibonacci.length; i++) {
+            fibonacci[i] = (fibonacci[i - 2]) + (fibonacci[i - 1]);
+        }
+
+        System.out.println(Arrays.toString(fibonacci));
+    }
+}
